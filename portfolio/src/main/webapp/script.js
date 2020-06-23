695c7c73973b6c29a388d54c104c3f19fe7828c8
Joymaneet Kaur <joymaneetkaur@berkeley.edu>
2020-06-23 03:22:58 -0700

    basic map working

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -83,3 +83,15 @@ async function getDataUsingAsyncAwait() {
   const data = await response.text();
   document.getElementById('data-container').innerText = data;
 }
+
+function createMap() {
+  const map = new google.maps.Map(
+      document.getElementById('map'),
+      {center: {lat: 37.422, lng: -122.084}, zoom: 16});
+}
+
+function init() {
+    createMap();
+    getdata();
+}
+
